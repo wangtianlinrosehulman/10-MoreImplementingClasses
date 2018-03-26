4a071eb87d342d005ebc3d6fb2cfe18adfd3ee78
wangt2 <wangt2@rose-hulman.edu>
2018-03-26 11:48:35 -0400

    Done

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -2,8 +2,8 @@
 A   Baby   class and methods that use the Baby class.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Tianlin Wang.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -90,11 +90,23 @@ def main():
 #
 ########################################################################
 
+class Baby (object):
+    def __init__(self, name):
+        self.t = 0
+        self.name = name
+        print('Hello baby', self.name,'!')
 
+    def feed_baby(self):
+        print('Thank you for feeding baby', self.name)
 
-
-
-
+    def hour_passes(self):
+        self.t += 1
+        if self.t == 1:
+            print('Baby', self.name, 'is sleeping')
+        if self.t ==2:
+            print('Baby', self.name, 'is awake. Time for food')
+        if self.t >= 3:
+            print('Baby', self.name, 'is CRYING uncontrollably!  Feed the Baby!')
 
 # ----------------------------------------------------------------------
 # If this module is running at the top level (as opposed to being
